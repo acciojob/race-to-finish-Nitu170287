d09be9180bbe553a9729f9170cd1d467a1f9244f
Nitu170287 <108494260+Nitu170287@users.noreply.github.com>
2023-05-24 22:11:47 -0400

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,40 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+const prom1 = new Promise((resolve, reject)=>{
+	setTimeout(()=>{
+		
+		resolve("One")},(Math.floor(Math.random() * 5000)))
+})
+
+const prom2 = new Promise((resolve, reject)=>{
+	setTimeout(()=>{
+		resolve("two")
+	},(Math.floor(Math.random() * 5000)))
+})
+
+const prom3 = new Promise((resolve, reject)=>{
+	setTimeout(()=>{
+		resolve("three")
+	},(Math.floor(Math.random() * 5000)));
+})
+
+const prom4 = new Promise((resolve, reject)=>{
+	setTimeout(()=>{
+		resolve("four")
+	},(Math.floor(Math.random() * 5000)))
+})
+
+const prom5 = new Promise((resolve, reject)=>{
+	setTimeout(()=>{
+		resolve("five")
+	},(Math.floor(Math.random() * 5000)))
+})
+
+Promise.any([prom1, prom2, prom3])
+.then((value)=>{
+	document.getElementById("output").innerText = value ;
+})
+.catch((err)=>{
+	console.log("error");
+})
